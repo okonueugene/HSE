00a7128f56e6f758bab15e837723e74d458bb74e
okonueugene <tylarrex67@gmail.com>
2024-04-05 15:00:36 +0300

    tasks controller

--- app/Http/Resources/TasksResource.php
+++ app/Http/Resources/TasksResource.php
@@ -15,6 +15,7 @@ class TasksResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'from' => $this->from,
