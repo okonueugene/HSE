362f64408b49c6b4e3516df34eaa16319fdb9bbd
okonueugene <tylarrex67@gmail.com>
2024-04-05 15:08:28 +0300

    tasks controller

--- app/Http/Controllers/Api/v1/TasksController.php
+++ app/Http/Controllers/Api/v1/TasksController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TasksResource;
 
 class TasksController extends Controller
 {
