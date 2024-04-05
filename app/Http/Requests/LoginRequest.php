635bab936dd6c2a5f0417b4480957e1e9c092704
okonueugene <tylarrex67@gmail.com>
2024-04-05 12:49:52 +0300

    api authentication

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -8,8 +8,10 @@ class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
-    public function authorize(): bool
+    public function authorize()
     {
         return true;
     }
@@ -17,13 +19,31 @@ class LoginRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, mixed>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-         'email' => 'required|string|email|max:255',
-         'password' => 'required|string|min:8',
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:8',
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email is required',
+            'email.email' => 'Email must be a valid email address',
+            'email.max' => 'Email must not be greater than 255 characters',
+            'password.required' => 'Password is required',
+            'password.min' => 'Password must be at least 8 characters',
+        ];
+    }
+    
 }
