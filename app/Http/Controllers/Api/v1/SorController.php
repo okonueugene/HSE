52e2c244600ff27612c1bb3f043c5b2891cdcd95
okonueugene <tylarrex67@gmail.com>
2024-04-18 13:56:01 +0300

    SOR update

--- app/Http/Controllers/Api/v1/SorController.php
+++ app/Http/Controllers/Api/v1/SorController.php
@@ -11,59 +11,45 @@ class SorController extends Controller
 {
     public function store(Request $request)
     {
+        $rules = [
+            'action_owner' => 'required',
+            'observation' => 'required',
+            'status' => 'required',
+            'steps_taken' => 'required',
+            'type_id' => 'required',
+        ];
+
+        $messages = [
+            'action_owner.required' => 'Action owner is required',
+            'observation.required' => 'Observation is required',
+            'status.required' => 'Status is required',
+            'steps_taken.required' => 'Steps taken is required',
+            'type_id.required' => 'Type is required',
+       
+        ];
+
+        $this->validate($request, $rules, $messages);
 
         try {
-            // Initialize the data array
-            $data = $request->input('_parts');
-
-            // [
-            //     [
-            //         "observation",
-            //         "This"
-            //     ],
-            //     [
-            //         "status",
-            //         "0"
-            //     ],
-            //     [
-            //         "steps_taken",
-            //         "That"
-            //     ],
-            //     [
-            //         "action_owner",
-            //         "Me"
-            //     ],
-            //     [
-            //         "assignor_id",
-            //         1
-            //     ],
-            //     [
-            //         "type_id",
-            //         "2"
-            //     ]
-            // ]
-
-            // Create a new SOR
+            // Create a new SOR record
             $sor = SOR::create([
-                'observation' => $data[0][1],
-                'status' => $data[1][1],
-                'steps_taken' => $data[2][1],
-                'action_owner' => $data[3][1],
-                'assignor_id' => $data[4][1],
-                'type_id' => $data[5][1],
+                'assignor_id' => $request->input('assignor_id'),
+                'action_owner' => $request->input('action_owner'),
+                'observation' => $request->input('observation'),
+                'status' => $request->input('status'),
+                'date' => date('Y-m-d'),
+                'steps_taken' => $request->input('steps_taken'),
+                'type_id' => $request->input('type_id'),
             ]);
 
-            //If there are images
+            // Handle file uploads
             if ($request->hasFile('images')) {
-                // Loop through the images
                 foreach ($request->file('images') as $image) {
-                    // Store the image
                     $sor->addMedia($image)->toMediaCollection('sor_images');
                 }
             }
 
-            // Return a success response
-            return response()->json(['message' => 'SOR created successfully'], 201);
+            return response()->json(['message' => 'SOR created successfully', 'data' => $sor], 200);
         } catch (\Exception $e) {
             // Handle any exceptions
             return response()->json(['error' => $e->getMessage()], 500);
@@ -78,5 +64,4 @@ class SorController extends Controller
 
         return response()->json($types);
     }
-
 }
