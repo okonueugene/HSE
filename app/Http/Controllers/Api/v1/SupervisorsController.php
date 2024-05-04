9f0be749ff75c004e1622e41d14e14c8282b359d
okonueugene <tylarrex67@gmail.com>
2024-05-04 14:11:16 +0300

    supervisors get

--- app/Http/Controllers/Api/v1/SupervisorsController.php
+++ app/Http/Controllers/Api/v1/SupervisorsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Models\Supervisor;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class SupervisorsController extends Controller
@@ -17,6 +18,26 @@ class SupervisorsController extends Controller
             , 200,
         ]);
 
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'designation' => 'required',
+        ]);
+
+        $supervisor = new Supervisor();
+        $supervisor->user_id = auth()->id();
+        $supervisor->name = $request->name;
+        $supervisor->date = date('Y-m-d');
+        $supervisor->designation = $request->designation;
+        $supervisor->save();
+
+        return response()->json([
+            'data' => $supervisor
+            , 200,
+        ]);
     }
 
 }
