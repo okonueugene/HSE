2cdad1c47ce67b7c2096abaf35965f2558f22af7
okonueugene <tylarrex67@gmail.com>
2024-05-04 13:36:18 +0300

    permits applicable post

--- app/Http/Controllers/Api/v1/PermitsApplicableController.php
+++ app/Http/Controllers/Api/v1/PermitsApplicableController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Controllers\Controller;
 use App\Models\Permit;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PermitsApplicableController extends Controller
 {
