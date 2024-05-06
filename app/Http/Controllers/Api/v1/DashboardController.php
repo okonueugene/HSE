c8aeef41b152544ec813801a6aec3d02b1603605
okonueugene <tylarrex67@gmail.com>
2024-05-06 12:36:58 +0300

    first responders

--- app/Http/Controllers/Api/v1/DashboardController.php
+++ app/Http/Controllers/Api/v1/DashboardController.php
@@ -12,33 +12,34 @@ use App\Models\Supervisor;
 use App\Models\Task;
 use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-         //initialize data array to hold the data
-         $data = array();
-         //Live Number Of People On Site
-         $data['attendance'] = User::where('last_login_at', '>=', Carbon::today())->count();
-         //Tasks Of The Day
-         $data['tasks'] = Task::where('created_at', '>=', Carbon::today())->count();
-         //Incidents Recorded
-         $data['incidents'] = Incident::where('created_at', '>=', Carbon::today())->count();
-         //Immediate Corrective Actions
-         $data['icas'] = Icas::where('created_at', '>=', Carbon::today())->count();
-         //Safety Observation Record
-         $data['sors'] = SOR::where('created_at', '>=', Carbon::today())->count();
-         // Supervisor
-         $data['supervisor'] = Supervisor::where('created_at', '>=', Carbon::today())->first()->name ?? 'No Supervisor Assigned';
-         // Personnel Present
-         $data['personells'] = PersonelPresent::where('created_at', '>=', Carbon::today())->first()->number ?? 0;
-         //Fire Marshal
-         $data['fire_marshal'] = FirstResponder::where('created_at', '>=', Carbon::today())->where('type', 'fire_marshal')->first()->name ?? 'No Fire Marshal Assigned';
-         //First Aider
-         $data['first_aider'] = FirstResponder::where('created_at', '>=', Carbon::today())->where('type', 'first_aider')->first()->name ?? 'No First Aider Assigned';
- 
+        //initialize data array to hold the data
+        $data = array();
+        //Live Number Of People On Site
+        $data['attendance'] = User::where('last_login_at', '>=', Carbon::today())->count();
+        //Tasks Of The Day
+        $data['tasks'] = Task::where('created_at', '>=', Carbon::today())->count();
+        //Incidents Recorded
+        $data['incidents'] = Incident::where('created_at', '>=', Carbon::today())->count();
+        //Immediate Corrective Actions
+        $data['icas'] = Icas::where('created_at', '>=', Carbon::today())->count();
+        //Safety Observation Record
+        $data['sors'] = SOR::where('created_at', '>=', Carbon::today())->count();
+        // Supervisor
+        $data['supervisor'] = Supervisor::where('created_at', '>=', Carbon::today())->first()->name ?? 'No Supervisor Assigned';
+        // Personnel Present
+        $data['personells'] = PersonelPresent::where('created_at', '>=', Carbon::today())->first()->number ?? 0;
+        //Fire Marshal
+        $data['fire_marshal'] = FirstResponder::where('created_at', '>=', Carbon::today())->where('type', 'fire_marshal')->first()->name ?? 'No Fire Marshal Assigned';
+        //First Aider
+        $data['first_aider'] = FirstResponder::where('created_at', '>=', Carbon::today())->where('type', 'first_aider')->first()->name ?? 'No First Aider Assigned';
+        //Permits Applicable
+        $data['permits'] = Permit::where('created_at', '>=', Carbon::today())->count() ?? 'No Permit Issued';
+
         return response()->json($data);
     }
 }
